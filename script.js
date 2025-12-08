b39f26d9637f65db047b1ec33a3314f3e6730744
cameron2025 <325392@student.nottinghamcollege.ac.uk>
2025-12-08 11:48:26 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-document.getElementById("clickMe").addEventListener("click", function() {
-  const colors = ["#FF5733", "#33FF57", "#3357FF", "#FFD700", "#FF33A6"];
-  const randomColor = colors[Math.floor(Math.random() * colors.length)];
-  document.body.style.background = `linear-gradient(135deg, ${randomColor}, #e0f7fa)`;
-  alert("Background changed! 🎨");
+document.getElementById("clickMeg").addEventListener("click", function() {
+    alert("Background changed! 🎨");
 });
